81b322f7742a6059c615407b2f773b40c618d756
aditya <adityajain773568@gmail.com>
2025-12-06 19:09:53 +0530

    Initial commit

---
+++ HashingProblems/src/Hashing01MaxMinFreq.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashing01MaxMinFreq {
+    public static void maxMinFreq(int []arr){
+        Map<Integer,Integer> hm = new HashMap<>();
+        int maxFreq,minFreq, maxEle,minEle = 0,0,Integer.MIN_VALUE,Integer.MAX_VALUE;
+    }
+
+    public static void main(String[] args) {
+        maxMinFreq(new int[]{3, 2, 3, 2, 4, 3});
+    }
+}
