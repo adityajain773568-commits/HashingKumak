597a20102bb1471e1c97788293c2496c8120db24
aditya <adityajain773568@gmail.com>
2025-12-06 19:33:44 +0530

    first question completed  commit

--- HashingProblems/src/Hashing01MaxMinFreq.java
+++ HashingProblems/src/Hashing01MaxMinFreq.java
@@ -2,9 +2,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Hashing01MaxMinFreq {
-    public static void maxMinFreq(int []arr){
-        Map<Integer,Integer> hm = new HashMap<>();
-        int maxFreq,minFreq, maxEle,minEle = 0,0,Integer.MIN_VALUE,Integer.MAX_VALUE;
+    public static void maxMinFreq(int[] arr) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        int maxFreq = 0, minFreq =1 ;
+        int maxEle = Integer.MIN_VALUE, minEle = Integer.MAX_VALUE;
+        for (int ele : arr) {
+            hm.put(ele, hm.getOrDefault(ele, 0) + 1);
+            if (hm.get(ele)>maxFreq){
+                maxFreq = hm.get(ele);
+                maxEle = ele;
+            }
+            if (hm.get(ele)<=minFreq){
+                minFreq = hm.get(ele);
+                minEle = ele;
+            }
+        }
+        System.out.println("maximum element is : " + maxEle +  " and its freq is : " + maxFreq);
+        System.out.println("minimum element is : " + minEle +  " and its freq is : " + minFreq);
     }
 
     public static void main(String[] args) {
